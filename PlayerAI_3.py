bc7319fd543ec50e0d7ed5dfc7a18bc870fb723e
mikhail <mikhail.b@skrumble.com>
2017-10-26 16:46:20 -0400

    add comments

--- PlayerAI_3.py
+++ PlayerAI_3.py
@@ -2,6 +2,24 @@ from random import randint
 from BaseAI_3 import BaseAI
 
 class PlayerAI(BaseAI):
+    
+# ALGORITHM:
+# getMove
+# minmax of moves
+#   max_func -> min_func -> max_func -> min_func ...
+#   keep track of depth or time (decison should be less than 0.2s)
+# apply alpha-beta pruning inside minmax
+# minimax should use heuristic function for options comparison
+
+# METHODS:
+# getMove: return best move (I hope) to GameManager
+# max_func: max of heuristic_func(move)
+# min_func: insert new tile, get moves, min of heuristic_func(moves)
+# alpha-beta: keep track of alpha and beta on the class self
+# heuristic_func: should include some best practices adding or removing weight of grid
+    
+# keep track of the time or depth on minmax
+    
         
     def getMove(self, grid):
         moves = grid.getAvailableMoves()
