fe52620a08696c41e2507b4b5c2168949063f668
Mikhail <mikhail.berezovskiy@gmail.com>
2017-10-31 10:09:15 -0400

    final_v1

--- PlayerAI_3.py
+++ PlayerAI_3.py
@@ -2,6 +2,7 @@ from random import randint
 from BaseAI_3 import BaseAI
 from Displayer_3  import Displayer
 import time
+import math
 
 class PlayerAI(BaseAI):
 
@@ -26,21 +27,19 @@ class PlayerAI(BaseAI):
 # keep track of the time or depth on minmax
 
     def getMove(self, grid):
-        self.t0 = time.clock()
-
-        # self.vals = []
         moves = grid.getAvailableMoves()
+        self.t0 = time.clock()
         self.lenmoves = len(moves)
         self.movenum = 0
+
         fn = 0
         best_move = moves[0]
         for move in moves:
             self.movenum += 1
-            self.expNodes = 0
             newGrid = grid.clone()
             newGrid.move(move)
-            move_val = self.minmax(newGrid, 10, -100000, 100000, False)
-            print ('expNodes:', self.expNodes)
+            move_val = self.minmax(newGrid, 5, -100000, 100000, False)
+            # print ('h=', move_val)
             if move_val > fn:
                 best_move = move
                 fn = move_val
@@ -51,10 +50,9 @@ class PlayerAI(BaseAI):
         ## searching implementation below
 
     def minmax(self, node, depth, alpha, beta, max_player):
-        self.expNodes += 1
         self.t1 = time.clock()
         # print ('time:', t1 - self.t0)
-        if depth == 0 or self.t1-self.t0 >= 0.18/self.lenmoves*self.movenum:
+        if depth == 0 or self.t1-self.t0 >= 0.15/self.lenmoves*self.movenum:
         # if depth == 0 or self.t1-self.t0 >= 0.15:
             return self.heuristic_func(node, depth)
 
@@ -83,7 +81,6 @@ class PlayerAI(BaseAI):
                 else:
                     insertVal = 2
                 childNode.insertTile(spawn, insertVal)
-
                 v = min(v, self.minmax(childNode, depth-1, alpha, beta, True))
                 beta = min (v, beta)
                 track += 1
@@ -93,40 +90,76 @@ class PlayerAI(BaseAI):
 
 
     def heuristic_func(self, grid, depth):
-        # heuristic_func should return value for particular move
-        # input: move, grid after move, some current state values
-        # return: weight for move
-        # 0-up, 1-down, 2-left, 3-right
 
         g = grid
-        # w = len(grid.getAvailableCells())/16
         gc = g.getAvailableCells()
         gm = grid.map
-        gridSum = 0
-        # countBig = 0
-        # gd = {}
         h = 0
-        for x in range(g.size):
-            for y in range(g.size):
+        m = g.getMaxTile()
+        for x in range(g.size-1):
+            for y in range(g.size-1):
                 v = gm[x][y]
-                gridSum += v
-                if x<3 and y<3:
-                    h += v - gm[x+1][y]
-                    h += v - gm[x][y+1]
-                    h += v - gm[x+1][y+1]
-                    # h += min(a,b,c)
+                i = x+1
+                j = y+1
 
-        # if countBig > 0:
-        #     h += gridSum/(countBig)
-        h += gridSum/(16-len(gc))
+                h+= v - gm[i][y]/4
+                h+= v - gm[x][j]
+                h+= v - gm[i][j]/2
 
-        # for v in gd:
-        #     print ('v:',v,' gdv:',gd[v], ' h:', h)
-        #     h += gd[v]/16 * v *2
-
-
-        # h *= w
-        # if gm[0][0] != g.getMaxTile():
-        #     h -= g.getMaxTile()
-
+        h+= 2*h/(16-len(gc))
+        if g.getMaxTile() != gm[0][0]:
+            h -= g.getMaxTile()
+        # h += len(gc) * h
         return h
+
+
+
+
+    # gd = {
+    #     0: 0,
+    #     2: 0,
+    #     4: 0,
+    #     8: 0,
+    #     16: 0,
+    #     32: 0,
+    #     64: 0,
+    #     256: 0,
+    #     128: 0,
+    #     512: 0,
+    #     1024: 0,
+    #     2048: 0,
+    #     4096: 0,
+    #     8192: 0
+    # }
+
+
+
+
+
+
+
+        # # print ('init d', gd)
+        # for i in gd:
+        #     if gd[i] != 0 and i not in [0,8192]:
+        #         # if i*2 <= gd[i]*i:
+        #         #     gd[i*2] += 1
+        #         #     gd[i] -= 2
+        #         # for j in range(gd[i]):
+        #         gd[i*2] += math.trunc(gd[i]*i/(i*2))
+        #         gd[i] = gd[i]%2
+        #         if gd[i] > 0:
+        #             target = [i] + target
+        # for i in range(16-len(target)):
+        #     # target = [0] + target
+        #     target.append(0)
+        #
+        # # target2 = source[:]
+        # # target2.sort()
+        # for i in range(len(source)):
+        #     h -= abs(target[i] - source[i])
+        #         # h += source[i] + target2[i]
+        # h += len(gc)
+        # print ('d', gd)
+        # print ('s', source)
+        # print ('t', target)
+        # print ('h', h)
